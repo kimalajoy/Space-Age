ca2d733cf82fdb903362a4a07c9781866c464b87
Megan Huggins <Muggins1265@gmail.com>
2020-05-28 20:38:26 -0600

    Add css transforms/transitions to hide and show pic info

--- src/components/BirthdayCard/BirthdayCard.vue
+++ src/components/BirthdayCard/BirthdayCard.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="birthday-card">
     <img v-bind:src="img" alt="">
+    <div id="nav-container">
+      <div class="button" tabindex="0">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </div>
+      <div id="nav-content" tabindex="0">
+        <h1>{{this.birthdayCard.explanation}}</h1>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -32,6 +42,80 @@
 
 .birthday-card img {
   width: 90%;
-  padding: 20px 0;
+  padding: 25px 0;
 }
+
+.button {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  z-index: 1;
+  border: 0;
+  background: transparent;
+  border-radius: 0;
+  height: 70px;
+  width: 30px;
+  cursor: pointer;
+  pointer-events: auto;
+  margin-left: 25px;
+  touch-action: manipulation;
+}
+
+.icon-bar {
+  display: block;
+  width: 100%;
+  height: 3px;
+  background: #aaa;
+  transition: .3s;
+}
+
+.icon-bar + .icon-bar {
+  margin-top: 5px;
+}
+
+#nav-container:focus-within .button {
+  pointer-events: none;
+}
+#nav-container:focus-within .icon-bar:nth-of-type(1) {
+  transform: translate3d(0,8px,0) rotate(45deg);
+}
+#nav-container:focus-within .icon-bar:nth-of-type(2) {
+  opacity: 0;
+}
+#nav-container:focus-within .icon-bar:nth-of-type(3) {
+  transform: translate3d(0,-8px,0) rotate(-45deg);
+}
+
+#nav-content {
+  opacity: 0;
+  /* margin-top: 25px; */
+  padding: 20px;
+  width: 90%;
+  /* height: 20px; */
+  max-width: 400px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  background: rgba(255, 255, 255, 0.75);
+  pointer-events: auto;
+  transform: translateX(-100%);
+  transition: all .3s;
+}
+
+#nav-content:hover {
+  background: rgba(255, 255, 255, 0.90);
+}
+
+#nav-content h1 {
+  font-size: 22px;
+}
+
+#nav-container:focus-within #nav-content {
+  transform: none;
+  opacity: 100%;
+  transition: all .3s;
+}
+
 </style>
